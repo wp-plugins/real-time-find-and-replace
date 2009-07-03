f96f0c0edc1e2c852b4438a96b715df37c2e8d68
Marios Alexandrou <Marios Alexandrou@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-03 13:01:47 +0000

    git-svn-id: https://plugins.svn.wordpress.org/real-time-find-and-replace/trunk@131726 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/jquery.dynamicfields.js
+++ js/jquery.dynamicfields.js
@@ -1,6 +1,6 @@
 function addFormField() {
 	var id = document.getElementById("id").value;
-	jQuery("#divTxt").append("<p id='row" + id + "'><label for='farfind" + id + "'>Find:&nbsp;</label><textarea rows='2' cols='40' name='farfind[" + id + "]' id='farfind" + id + "' /></textarea>&nbsp;&nbsp;<label for='farregex" + id + "'>RegEx?:&nbsp;</label><input type='checkbox' name='farregex[" + id + "]' id='farregex" + id + "' />&nbsp;&nbsp;<label for='farreplace" + id + "'>Replace:&nbsp;</label><textarea rows='2' cols='40' name='farreplace[" + id + "]' id='farreplace" + id + "' /></textarea>&nbsp;&nbsp&nbsp;<a href='#' onClick='removeFormField(\"#row" + id + "\"); return false;'>Remove</a></p>");
+	jQuery("#divTxt").append("<p id='row" + id + "'><label for='farfind" + id + "'>Find:&nbsp;</label><textarea rows='2' cols='40' name='farfind[" + id + "]' id='farfind" + id + "' /></textarea>&nbsp;&nbsp;<label for='farregex" + id + "'>RegEx?:&nbsp;</label><input type='checkbox' name='farregex[" + id + "]' id='farregex" + id + "' />&nbsp;&nbsp;<label for='farreplace" + id + "'>Replace:&nbsp;</label><textarea rows='2' cols='30' name='farreplace[" + id + "]' id='farreplace" + id + "' /></textarea>&nbsp;&nbsp&nbsp;<a href='#' onClick='removeFormField(\"#row" + id + "\"); return false;'>Remove</a></p>");
 	jQuery('#row' + id).highlightFade({
 		speed:1000
 	});
